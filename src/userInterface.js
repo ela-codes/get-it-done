a721a7b117d981967e453a010e56c5c4722318f2
ateodocio <teodocio.am@gmail.com>
2023-03-11 12:51:02 -0600

    Fix checkbox styling

--- src/userInterface.js
+++ src/userInterface.js
@@ -19,11 +19,12 @@ function showNewTask(currTask) {
 
         const label = document.createElement('label');
         label.setAttribute('for', currTask.currID);
+        label.innerHTML = currTask.currTitle
     
-        const labelSpan = document.createElement('span');
-        labelSpan.innerHTML = currTask.currTitle
+        // const labelSpan = document.createElement('span');
+        // labelSpan.innerHTML = currTask.currTitle
 
-        label.appendChild(labelSpan)
+        // label.appendChild(labelSpan)
 
         const span = document.createElement('span');
         span.setAttribute('class', 'due-date');
